bbfc5dfcfd4fd303cdddd0496be3b3212b9dbe19
Michal <michal@gmail.com>
2022-02-13 22:31:57 +0000

    doing s3

--- start.py
+++ start.py
@@ -12,7 +12,7 @@ parser.add_argument("--w", type = str, default = "ui")
 parser.add_argument("--k", type = int, default = 3)
 parser.add_argument("--s", type = bool, default = True)
 parser.add_argument("--s3", type = bool, default = True)
-parser.add_argument("--kflods", type = int, default = 1)
+parser.add_argument("--kfolds", type = int, default = 1)
 args = parser.parse_args()
 
 
@@ -20,7 +20,7 @@ ic(args.w)
 ic(args.k)
 ic(args.s)
 ic(args.s3)
-ic(args.kflods)
+ic(args.kfolds)
 
 
-run_experiment(k = args.k, which = args.w, save_results = args.s, save_in_s3=args.s3, kfolds = args.kflods)
+run_experiment(k = args.k, which = args.w, save_results = args.s, save_in_s3=args.s3, kfolds = args.kfolds)
